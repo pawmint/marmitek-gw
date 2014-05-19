4cdefcb5b55b5a8955019862bac6f2aa1f8ec6d2
Romain Endelin <romain.endelin@gmail.com>
2014-05-19 14:20:23 +0200

    Integrated mochad from reading socket

--- marmitek-gw.py
+++ marmitek-gw.py
@@ -1,27 +1,11 @@
 #!/usr/bin/env python
 
-import sys
 import logging
+
+from marmitek import mochad_reader
 
 from ubigate import Ubigate
 from ubigate import log, logger
-from marmitek.sensors import motion_signal, door_signal
-
-
-def get_signal():
-    return sys.stdin.readline()
-
-
-def gather_data(signal):
-    signal_types = [motion_signal, door_signal]
-
-    data = None
-
-    for checker in signal_types:
-        data = checker.matches(signal)
-        if data is not None:
-            break
-    return data
 
 
 def main():
@@ -38,16 +22,9 @@ def main():
                                   gate.config.house,
                                   gate.config.username))
 
-    while True:
-        signal = get_signal()
-        logger.debug('Signal received: %s' % signal)
-        try:
-            sensor, data = gather_data(signal)
-            topic = "/marmitek/%s" % sensor
-
-            gate.push(topic, data)
-        except TypeError:
-            pass
+    for sensor, data in mochad_reader.run():
+        topic = "/marmitek/%s" % sensor
+        gate.push(topic, data)
 
 
 if __name__ == "__main__":
---
+++ marmitek/mochad_reader.py
@@ -0,0 +1,42 @@
+import socket
+from ubigate import logger
+
+from marmitek.sensors import motion_signal, door_signal
+
+# TODO Put the config in the config file
+MOCHADHOST = "127.0.0.1"
+MOCHADPORT = 1099
+
+Sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+Sock.connect((MOCHADHOST, MOCHADPORT))
+
+
+def read_from_mochad():
+    data = Sock.recv(1024)
+    line = repr(data).strip("b'")
+    lines = line.split('\\n')
+    return lines
+
+
+def gather_data(signal):
+    signal_types = [motion_signal, door_signal]
+
+    data = None
+
+    for checker in signal_types:
+        data = checker.matches(signal)
+        if data is not None:
+            break
+    return data
+
+
+def run():
+    while True:
+        lines = read_from_mochad()
+        for signal in lines[:-1]:
+            logger.debug('Signal received: %s' % signal)
+            try:
+                sensor, data = gather_data(signal)
+                yield sensor, data
+            except TypeError:
+                pass
